cd99d0daf20f4a6c78aa1440f2ddb4a171d9c775
kedorev <kedorev@gmail.com>
2018-04-11 15:00:40 +0200

    update change folder

---
+++ class/Database.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apprenant
+ * Date: 11/04/18
+ * Time: 10:27
+ */
+
+abstract class Database
+{
+    private static $const;
+
+    private static function makeConnection()
+    {
+        $user = 'root';
+        $pass = 'admin';
+        $database = 'hack';
+
+        return new PDO('mysql:host=localhost;dbname='.$database, $user, $pass);
+    }
+
+    public static function getConnection()
+    {
+        if(!(self::$const))
+        {
+            self::$const = self::makeConnection();
+        }
+        return self::$const;
+    }
+
+}
